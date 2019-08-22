f4f4867a3dd7859c63a73bedd86cdda8bb85f371
Михаил <mikhail-gankin@yandex.ru>
2019-08-22 21:58:39 +0300

    pixel perfect

---
+++ source/js/popup.js
@@ -0,0 +1,4 @@
+var wrong = document.querySelector('.wrong');
+var wrongClose = document.querySelector('.wrong__button');
+var success = document.querySelector('.success');
+var successClose = document.querySelector('.success__button');
