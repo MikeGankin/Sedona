c46485d35ed4493f89b7c43135ff2a8ac5dd5f95
Михаил <mikhail-gankin@yandex.ru>
2019-08-31 13:16:14 +0300

    form validate

--- source/js/main.js
+++ source/js/main.js
@@ -12,23 +12,23 @@ if (map) {
   var myMap;
   ymaps.ready(init);
 
-function init() {
-   myMap = new ymaps.Map('map_canvas', {
-   center: [59.93863106417265, 30.3230545],
-   zoom: 17,
-   controls: ['zoomControl'],
- }, {
-    searchControlProvider: 'yandex#search'
- });
-
-  myMap.geoObjects
-    .add(new ymaps.Placemark([59.93863106417265, 30.3230545], {
-      balloonContent: 'цвет <strong>голубой</strong>',
-      iconCaption: 'ул. Б. Конюшенная, д. 19/8'
+  function init() {
+    myMap = new ymaps.Map('map_canvas', {
+      center: [59.93863106417265, 30.3230545],
+      zoom: 17,
+      controls: ['zoomControl'],
     }, {
-      preset: 'islands#dotIconWithCaption',
-      iconCaptionMaxWidth: '200'
-    }));
+      searchControlProvider: 'yandex#search'
+    });
+
+    myMap.geoObjects
+      .add(new ymaps.Placemark([59.93863106417265, 30.3230545], {
+        balloonContent: 'цвет <strong>голубой</strong>',
+        iconCaption: 'ул. Б. Конюшенная, д. 19/8'
+      }, {
+        preset: 'islands#dotIconWithCaption',
+        iconCaptionMaxWidth: '200'
+      }));
   }
 }
 
@@ -38,32 +38,29 @@ var phoneInput = form.querySelector("[name=phone]");
 var emailInput = form.querySelector("[name=email]");
 
 try {
-    storage = localStorage.getItem("nameInput");
-    storage = localStorage.getItem("surnameInput");
-    storage = localStorage.getItem("phoneInput");
-    storage = localStorage.getItem("emailInput");
-  } catch (err) {
-    isStorageSupport = false;
-  }
+  storage = localStorage.getItem("nameInput");
+  storage = localStorage.getItem("surnameInput");
+  storage = localStorage.getItem("phoneInput");
+  storage = localStorage.getItem("emailInput");
+} catch (err) {
+  isStorageSupport = false;
+}
 
-submit.addEventListener("click", function(evt) {
+form.addEventListener("submit", function(evt) {
+  if (!nameInput.value || !surnameInput.value || !phoneInput.value || !emailInput.value) {
     evt.preventDefault();
-    wrong.classList.add("modal-show");
-
-    if (storage) {
-      nameInput.value = storage;
-      surnameInput.focus();
-    } else {
-      nameInput.focus();
+      wrong.classList.add("modal-show");
+  } else {
+    return true;
     }
-  });
+});
 
 ok.addEventListener("click", function(evt) {
   evt.preventDefault();
   wrong.classList.remove("modal-show");
-} )
+});
 
 closeWindow.addEventListener("click", function(evt) {
   evt.preventDefault();
   success.classList.remove("modal-show");
-} )
+});
