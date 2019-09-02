66e21e6a6f98efe2416bf7c64957e4b2112e23fe
Михаил <mikhail-gankin@yandex.ru>
2019-09-02 20:21:34 +0300

    img optimization

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,36 @@
 "use strict";
 
 var gulp = require("gulp");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
 var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
+var rename = require("gulp-rename");
+var svgstore = require("gulp-svgstore");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+
+
 var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var csso = require("gulp-csso");
 var server = require("browser-sync").create();
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(posthtml())
+    .pipe(gulp.dest("source"));
+});
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(less())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
+    .pipe(postcss([ autoprefixer() ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
@@ -32,6 +47,35 @@ gulp.task("server", function () {
 
   gulp.watch("source/less/**/*.less", gulp.series("css"));
   gulp.watch("source/*.html").on("change", server.reload);
+});
+
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3})
+]))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("sprite", function () {
+  return gulp.src("source/img/icon-*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(posthtml())
+    .pipe(gulp.dest("source"));
 });
 
 gulp.task("start", gulp.series("css", "server"));
--- source/js/main.js
+++ source/js/main.js
@@ -11,74 +11,25 @@ var nav = document.querySelector('.main-nav');
 var menuClose = document.querySelector('.main-nav__close');
 var menuOpen = document.querySelector('.header-wrapper__nav-open');
 
+var nameInput = document.querySelector("[name=name]");
+var surnameInput = document.querySelector("[name=surname]");
+var phoneInput = document.querySelector("[name=phone]");
+var emailInput = document.querySelector("[name=email]");
 
-// if (map) {
-//   var myMap;
-//   ymaps.ready(init);
-//
-//   function init() {
-//     myMap = new ymaps.Map('map_canvas', {
-//       center: [59.93863106417265, 30.3230545],
-//       zoom: 17,
-//       controls: ['zoomControl'],
-//     }, {
-//       searchControlProvider: 'yandex#search'
-//     });
-//
-//     myMap.geoObjects
-//       .add(new ymaps.Placemark([59.93863106417265, 30.3230545], {
-//         balloonContent: 'цвет <strong>голубой</strong>',
-//         iconCaption: 'ул. Б. Конюшенная, д. 19/8'
-//       }, {
-//         preset: 'islands#dotIconWithCaption',
-//         iconCaptionMaxWidth: '200'
-//       }));
-//   }
-// }
+try {
+  storage = localStorage.getItem("nameInput");
+  storage = localStorage.getItem("surnameInput");
+  storage = localStorage.getItem("phoneInput");
+  storage = localStorage.getItem("emailInput");
+} catch (err) {
+  isStorageSupport = false;
+}
 
-nav.classList.add("menu-hide");
-
-menuOpen.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  nav.classList.remove("menu-hide");
-  nav.classList.add("menu-show");
+form.addEventListener("submit", function(evt) {
+  if (!nameInput.value || !surnameInput.value || !phoneInput.value || !emailInput.value) {
+    evt.preventDefault();
+      wrong.classList.add("modal-show");
+  } else {
+    return true;
+    }
 });
-
-menuClose.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  nav.classList.remove("menu-show");
-  nav.classList.add("menu-hide");
-});
-
-// var nameInput = document.querySelector("[name=name]");
-// var surnameInput = document.querySelector("[name=surname]");
-// var phoneInput = document.querySelector("[name=phone]");
-// var emailInput = document.querySelector("[name=email]");
-//
-// try {
-//   storage = localStorage.getItem("nameInput");
-//   storage = localStorage.getItem("surnameInput");
-//   storage = localStorage.getItem("phoneInput");
-//   storage = localStorage.getItem("emailInput");
-// } catch (err) {
-//   isStorageSupport = false;
-// }
-//
-// form.addEventListener("submit", function(evt) {
-//   if (!nameInput.value || !surnameInput.value || !phoneInput.value || !emailInput.value) {
-//     evt.preventDefault();
-//       wrong.classList.add("modal-show");
-//   } else {
-//     return true;
-//     }
-// });
-//
-// ok.addEventListener("click", function(evt) {
-//   evt.preventDefault();
-//   wrong.classList.remove("modal-show");
-// });
-//
-// closeWindow.addEventListener("click", function(evt) {
-//   evt.preventDefault();
-//   success.classList.remove("modal-show");
-// });
